9f55989b6a9e734d9f6167d8455c6b50601865d4
João Victor <joao.victor@internit.net.br>
2025-06-11 23:53:54 -0300

    postgresql and docker classe has been added

--- PostgresqlAndDocker/src/index.js
+++ PostgresqlAndDocker/src/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import pool from './config/db.js';
+
+import userRoutes from './routes/userRoutes.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 dotenv.config();
 
@@ -8,7 +12,17 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
+
+app.get("/", async(req, res) => {
+    const result = await pool.query("SELECT current_database()");
+    res.send(`The database name is ${result.rows[0].current_database}`);
+});
+
+app.use("/api", userRoutes);
+
+app.use(errorHandler);
+
 
 app.listen(port, () => {
     console.log('Server is now listening on', port);
